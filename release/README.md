d8570fc3cc8fc7cb292c147b10621fa2b53193de
teamcons_atwork <littlepastelworld@gmail.com>
2024-06-12 15:59:25 +0200

    release folder

---
+++ release/README.md
@@ -0,0 +1,64 @@
+
+
+# 💽 Assistant
+
+### All the stuff
+
+A small utility which offers several functions lacking for windows
+
+### Done and tested
+- Keep awake: Toggle between preventing computer to go to sleep or not
+- Hot corner top left : Hitting the top left edge triggers an overview
+- Hot corner hover meta : Hitting the botton left (windows logo) opens the start menu
+- Autostart : Start Assistant with Windows
+
+
+### Todo, maybe, idk
+- Clipboard manager: Save the last X items in clipboard
+- Clipboard manager: Use several items from clipboard by pasting in quick succession
+- Timer ? Ask to have a notification after X time
+- Localization ?
+
+
+  <table align="center" border="none">
+      <td><img src="https://github.com/teamcons/Assistant/blob/main/images/Screenshot.png"" /></td>
+</table>
+
+
+# Documentation
+
+### Hot corner: Top left
+The program constantly checks whether the mouse is in the top left
+Simulate Meta + D
+
+### Hot corner: Hover meta
+
+The program constantly checks whether the mouse is in the bottom
+Simulates Meta
+
+### Keep awake
+
+The computer is prevented to go to sleep by simulating a keypress on F15.
+This key shouldnt be used by any program nor appear on the keyboard but... If there is weird behavior it could come from here.
+
+
+
+
+
+# Acknowledgments
+
+
+
+Had a look through various sources for having a tray icon. Thanks to all of these !
+- https://github.com/damienvanrobaeys/Build-PS1-Systray-Tool
+- https://www.systanddeploy.com/2018/12/create-your-own-powershell.html
+- https://stackoverflow.com/questions/54649456/powershell-notifyicon-context-menu
+- https://adamtheautomator.com/powershell-async/
+
+The icons are from the same website... I should do my owns. Thanks nonetheless !
+
+The soda can icon
+<a href="https://www.flaticon.com/free-icons/soda" title="soda icons">Soda icons created by Chanut-is-Industries - Flaticon</a>
+
+
+
---
+++ release/functionalities/hotcorner_winbutton.ps1
@@ -0,0 +1,82 @@
+#================================================================================================================================
+
+#----------------INFO----------------
+#
+# CC-BY-SA-NC Stella Ménier <stella.menier@gmx.de>
+# Project creator for Skrivanek GmbH
+#
+# Usage: powershell.exe -executionpolicy bypass -file ".\Rocketlaunch.ps1"
+# Usage: Compiled form, just double-click.
+#
+#-------------------------------------
+
+
+    #===============================================
+    #                Initialization                =
+    #===============================================
+
+#========================================
+# Get all important variables in place 
+
+param(
+    [int]$hotcorner_reactivity              = 500,   # In milliseconds - How often to check mouse location.
+    [byte]$hotcorner_sensitivity            = 50    # In pixels - Size of hot corner area
+    )
+
+# Calculate positions of the hot corner area
+[int]$Bottom                            = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height
+[int]$BottomInRange                     = ($Bottom - $hotcorner_sensitivity)
+
+
+# We check a bit further on X, because the button is off center
+# This way the hot corner is a real "hover over windows logo" button
+[int]$RightSide                         = ($hotcorner_sensitivity + 5)
+
+# Imports
+Add-Type -AssemblyName System.Windows.Forms
+
+
+
+    #=========================================
+    #                MAINLOOP                =
+    #=========================================
+
+#========================================
+# The test is Forever
+while ($true)
+{
+
+    # If in the corner. We test for a range, because monitors on the left have negative X, monitors on top negative Y
+    # "0" is the absolute corner of main screen.
+    if (([Windows.Forms.Cursor]::Position.X -In 0..$RightSide ) -and
+        ([Windows.Forms.Cursor]::Position.Y -In $BottomInRange..$Bottom))
+    {
+        # Trigger Windows + Tab (Overview)
+        Write-Output "[HOT CORNER] Activated!"
+        #[Windows.Forms.Cursor]::Position
+        [System.Windows.Forms.SendKeys]::SendWait('^{ESC}')
+
+        
+        # wait for people to leave the button
+        Write-Output "[HOT CORNER] Wait for rearming..."
+        while   (([Windows.Forms.Cursor]::Position.X -In 0..$RightSide ) -and
+                ([Windows.Forms.Cursor]::Position.Y -In $BottomInRange..$Bottom))
+        {
+            Start-Sleep -Milliseconds $hotcorner_reactivity
+        }
+        Write-Output "[HOT CORNER] Ready!"
+
+    } # End of if hits the corner
+    
+
+
+    # If not in the corner, wait.
+    # This could be without "else", with the effect for the hot corner to be slightly less reactive just after rearming
+    else {
+        # Wait before rechecking again.
+        # Else we'd just burn the cpu checking constantly
+        Start-Sleep -Milliseconds $hotcorner_reactivity
+    }
+
+
+}
