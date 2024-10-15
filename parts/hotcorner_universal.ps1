bb38103ac9eac670a9a4de55192bcb2fdaafbbc5
teamcons_atwork <littlepastelworld@gmail.com>
2024-10-15 11:39:01 +0200

    universal

---
+++ parts/hotcorner_universal.ps1
@@ -0,0 +1,224 @@
+#================================================================================================================================
+
+#----------------INFO----------------
+#
+# CC-BY-SA-NC Stella Ménier <stella.menier@gmx.de>
+# Project creator for Skrivanek GmbH
+#
+# Usage: powershell.exe -executionpolicy bypass -file ".\Rocketlaunch.ps1"
+# Usage: Compiled form, just double-click.
+#
+#-------------------------------------
+
+
+    #===============================================
+    #                Initialization                =
+    #===============================================
+
+#========================================
+# Get all important variables in place 
+
+param(
+    [string]$hotcorner_where                = "topleft",
+    [string]$hotcorner_what                 = "overview",
+    [int]$hotcorner_reactivity              = 400,   # In milliseconds - How often to check mouse location.
+    [byte]$hotcorner_sensitivity            = 30    # In pixels - Size of hot corner area
+    )
+
+
+# Imports
+Add-Type -AssemblyName System.Windows.Forms
+
+
+
+    #==========================================
+    #                Functions                =
+    #==========================================
+
+#========================================
+# This is very ugly, but we cannot send "Meta + Tab" otherwise
+# We basically code in another language what we need, and slorp it in our code as a function
+#https://www.itcodar.com/csharp/sending-windows-key-using-sendkeys.html
+$source = @"
+using System;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace KeySends
+{
+    public class KeySend
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        private const int KEYEVENTF_EXTENDEDKEY = 1;
+        private const int KEYEVENTF_KEYUP = 2;
+        public static void KeyDown(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY, 0);
+        }
+        public static void KeyUp(Keys vKey)
+        {
+            keybd_event((byte)vKey, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+    }
+}
+"@
+Add-Type -TypeDefinition $source -ReferencedAssemblies "System.Windows.Forms"
+
+
+
+
+# Take where the user wants their hotcorner
+# Calculate from there the screen region to keep watch of
+switch ($hotcorner_where)
+{
+    "topleft" {
+        $X_from     = 0
+        $X_to       = $hotcorner_sensitivity
+        $Y_from     = 0
+        $Y_to       = $hotcorner_sensitivity
+
+      }
+    "topright" {
+        $X_from     = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width - $hotcorner_sensitivity
+        $X_to       = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width
+        $Y_from     = 0
+        $Y_to       = $hotcorner_sensitivity
+
+      }
+    "bottomright" {
+        $X_from     = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width - $hotcorner_sensitivity
+        $X_to       = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width
+        $Y_from     = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height - $hotcorner_sensitivity
+        $Y_to       = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height
+      }
+    "bottomleft" {
+        $X_from     = 0
+        $X_to       = $hotcorner_sensitivity
+        $Y_from     = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height - $hotcorner_sensitivity
+        $Y_to       = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height
+
+      }
+}
+
+
+
+# Take what the user want to activate
+# Define key combos for it
+# We dont want a function which tests each time everything, thats useless
+# So we define a function depending on what we need
+switch ($hotcorner_what) {
+    "overview" {
+        function action {
+            [KeySends.KeySend]::KeyDown("LWin")
+            [KeySends.KeySend]::KeyDown("Tab")
+            [KeySends.KeySend]::KeyUp("LWin")
+            [KeySends.KeySend]::KeyUp("Tab")
+        }
+    }
+    "winbutton" {
+        function action {
+            [System.Windows.Forms.SendKeys]::SendWait('^{ESC}')
+        }
+    }
+    "showdesktop" {
+        function action {
+            [KeySends.KeySend]::KeyDown("LWin")
+            [KeySends.KeySend]::KeyDown("D")
+            [KeySends.KeySend]::KeyUp("LWin")
+            [KeySends.KeySend]::KeyUp("D")
+        }
+    }
+
+    "desktopright" {
+        function action {
+            [KeySends.KeySend]::KeyDown("LWin")
+            [KeySends.KeySend]::KeyDown("^")
+            [KeySends.KeySend]::KeyDown("RIGHT")
+            [KeySends.KeySend]::KeyUp("LWin")
+            [KeySends.KeySend]::KeyUp("^")
+            [KeySends.KeySend]::KeyUp("RIGHT")            
+        }
+    }
+
+    "desktopleft" {
+        function action {
+            [KeySends.KeySend]::KeyDown("LWin")
+            [KeySends.KeySend]::KeyDown("^")
+            [KeySends.KeySend]::KeyDown("LEFT")
+            [KeySends.KeySend]::KeyUp("LWin")
+            [KeySends.KeySend]::KeyUp("^")
+            [KeySends.KeySend]::KeyUp("LEFT")            
+        }
+    }
+
+
+    "closewindow" {
+        function action {
+            [System.Windows.Forms.SendKeys]::SendWait('%{F4}')
+        }
+    }
+
+    "showclipboard" {
+        function action {
+            [KeySends.KeySend]::KeyDown("LWin")
+            [KeySends.KeySend]::KeyDown("V")
+            [KeySends.KeySend]::KeyUp("LWin")
+            [KeySends.KeySend]::KeyUp("V")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+    #=========================================
+    #                MAINLOOP                =
+    #=========================================
+
+#========================================
+# The test is Forever
+while ($true)
+{
+
+    # If in the corner. We test for a range, because monitors on the left have negative X, monitors on top negative Y
+    # "0" is the absolute corner of main screen.
+    if (([Windows.Forms.Cursor]::Position.X -In $X_from..$X_to) -and
+        ([Windows.Forms.Cursor]::Position.Y -In $Y_from..$Y_to))
+    {
+        # Trigger
+        Write-Output "[HOT CORNER] Activated!"
+        action
+
+
+        # wait for people to leave the button
+        # Else depending on reactivity we'd just spam the keypress
+        Write-Output "[HOT CORNER] Wait for rearming..."
+        while   (([Windows.Forms.Cursor]::Position.X -In $X_from..$X_to) -and
+                ([Windows.Forms.Cursor]::Position.Y -In $Y_from..$Y_to))
+        {
+            Start-Sleep -Milliseconds $hotcorner_reactivity
+        }
+        Write-Output "[HOT CORNER] Ready!"
+
+
+
+    } # End of if hits the corner
+
+
+    # If not in the corner, wait.
+    # This could be without "else", with the effect for the hot corner to be slightly less reactive just after rearming
+    else {
+            # Wait before rechecking again.
+            # Else we'd just burn the cpu checking constantly
+            Start-Sleep -Milliseconds $hotcorner_reactivity
+    }
+
+
+
+
+} # End of testing forever without end
